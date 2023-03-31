a1ebbf9a4f5867bee7c4744e8337a747a13db769
bruno <bruno.ams@hotmail.com>
2023-03-31 10:56:09 -0300

    Mongo Connect Repository e Service

--- src/main/java/com/example/mongodb/resource/UserResource.java
+++ src/main/java/com/example/mongodb/resource/UserResource.java
@@ -4,28 +4,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.mongodb.domain.User;
+import com.example.mongodb.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		User pessoa1 = new User(1, "maria", "maria@uol.com");
-		User pessoa2 = new User(2, "jose", "jose@uol.com");
-		User pessoa3 = new User(3, "benedito", "benedito@uol.com");
-		
-		List<User> usuarios = new ArrayList<>();
-		usuarios.addAll(Arrays.asList(pessoa1, pessoa2, pessoa3));
-		
-		return ResponseEntity.ok().body(usuarios);
-		
+	public ResponseEntity<List<User>> findAll(){		
+		List<User> usuarios = service.findAll();		
+		return ResponseEntity.ok().body(usuarios);		
 	}
 
 }
