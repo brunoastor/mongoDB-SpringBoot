2ad32cb47cd273fd116e04c17ce95969f1a5955b
bruno <bruno.ams@hotmail.com>
2023-03-31 14:15:36 -0300

    GetMapping por ID

--- src/main/java/com/example/mongodb/services/UserService.java
+++ src/main/java/com/example/mongodb/services/UserService.java
@@ -1,20 +1,27 @@
 package com.example.mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.mongodb.domain.User;
 import com.example.mongodb.repository.UserRepository;
+import com.example.mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 	
 	@Autowired
-	private UserRepository rep;
+	private UserRepository repo;
 	
 	public List<User> findAll(){
-		return rep.findAll();
+		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));		
 	}
 }
