8ee6879fbb5d48a8a968a9eb9eeb05cf8c4afdd8
bruno <bruno.ams@hotmail.com>
2023-04-13 21:14:14 -0300

    @DBRef (user post ref) - port:8081

--- src/main/java/com/example/mongodb/domain/config/Instantiation.java
+++ src/main/java/com/example/mongodb/domain/config/Instantiation.java
@@ -42,6 +42,9 @@ public class Instantiation implements CommandLineRunner {
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
+		user1.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(user1);
+		
 	}
 
 }
