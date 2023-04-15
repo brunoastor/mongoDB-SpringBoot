f342ae380a7c9b02c221bdbb17b7d2857eb8c571
bruno <bruno.ams@hotmail.com>
2023-04-15 18:27:18 -0300

    @Query

--- src/main/java/com/example/mongodb/repository/PostRepository.java
+++ src/main/java/com/example/mongodb/repository/PostRepository.java
@@ -3,7 +3,7 @@ package com.example.mongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.mongodb.domain.Post;
@@ -13,5 +13,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 }
